3070727109db88e86ec9c9a17f47e7e627d76d81
tahenyBELGUITH <taheny.belguith@gmail.com>
2022-11-05 10:47:39 +0100

    Add data to db

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,16 @@
 
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments
+  has_many :likes
+
+  def five_recent_comments
+    post.comments.last(5)
+  end
+
+  def update_posts_counter
+    post.user.increment!(:posts_counter)
+  end
+  
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
-# frozen_string_literal: true
-
 class User < ApplicationRecord
+ has_many :comments 
+ has_many :posts
+ has_many :likes
+
+ def three_recent_posts  
+  user.posts.last(3)
+ end
+
 end
