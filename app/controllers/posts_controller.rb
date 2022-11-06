3951f0adae0ac29d0159f2d0ade77978b7388ef1
tahenyBELGUITH <taheny.belguith@gmail.com>
2022-11-06 08:53:44 +0100

    Add all the views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,55 @@
 class PostsController < ApplicationController
- def index 
+  before_action :set_post, only: %i[show destroy edit update]
+
+ def index
+   @user = User.find(params[:user_id])
+   @posts = @user.posts
  end
 
  def show 
+   @user = User.find(params[:user_id])
  end
+
+ def new 
+  @post = Post.new 
+ end
+
+ def create 
+  @post = Post.new(post_params)
+  @post.comments_counter = 0
+  @user = User.find(params[:user_id])
+  if @post.save 
+   redirect_to user_posts_path
+  else 
+   render :new
+  end
+ end
+
+ def edit 
+   @user = User.find(params[:user_id])
+ end
+
+ def update 
+  if @post.update(post_params)
+    redirect_to user_posts_path
+  else
+    render :edit
+  end
+
+ end
+
+ def destroy 
+   @post.destroy
+   redirect_to user_posts_path
+ end
+
+ private 
+ def set_post
+  @post = Post.find(params[:id])
+ end
+
+ def post_params
+   params.require(:post).permit(:title, :text)
+ end
+ 
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,6 +4,8 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
+  after_save :update_comments_counter
+
 def update_comments_counter
   comment.post.increment!(:comments_counter)
 end
